b8d4f7925df64f9187ef3388673b654ff05ceed1
jlzbrowning <jlzbrowning@gmail.com>
2023-03-21 06:03:45 -0700

    h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@
  *       used in the 0x02-functions_nested_loops directory.
  */
 
-int _putchar(char* c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
