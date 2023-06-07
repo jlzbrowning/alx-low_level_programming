dc4b85a56777b093976cc071f96264b7ad1e9d73
jlzbrowning <jlzbrowning@gmail.com>
2023-06-07 05:13:48 -0700

    Script commit from gits.sh

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /** 
- * _puts_recuriosn - Prints a given string, followed by a new line.
+ * _puts_recursion - Prints a given string, followed by a new line.
  * @s: The string to be printed.
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s)
