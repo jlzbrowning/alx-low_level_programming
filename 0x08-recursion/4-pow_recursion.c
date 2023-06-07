b12daa9094416a7b9951d2cc267f1c30c2ef2d22
jlzbrowning <jlzbrowning@gmail.com>
2023-06-07 06:06:42 -0700

    Script commit from gits.sh

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns x raised to power y
+ * @x: number to raise
+ * @y: power to raise by
+ * Return: x raised to y
+ */
+int _pow_recursion(int , int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
