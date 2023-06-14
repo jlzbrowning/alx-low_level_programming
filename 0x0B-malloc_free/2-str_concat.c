54a37feffaf94eb5c0221696b5da1f0a472be456
jlzbrowning <jlzbrowning@gmail.com>
2023-06-14 06:50:58 -0700

    Script commit from gits.sh

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2) {
+    if (s1 == NULL) {
+        s1 = "";
+    }
+    if (s2 == NULL) {
+        s2 = "";
+    }
+
+    unsigned int s1_length = 0, s2_length = 0;
+    while (s1[s1_length] != '\0') {
+        s1_length++;
+    }
+    while (s2[s2_length] != '\0') {
+        s2_length++;
+    }
+
+    char *concatenated = (char *) malloc(sizeof(char) * (s1_length + s2_length + 1));
+    if (concatenated == NULL) {
+        return NULL; // Allocation failed
+    }
+
+    unsigned int i, j;
+
+    for (i = 0; i < s1_length; i++) {
+        concatenated[i] = s1[i];
+    }
+
+    for (j = 0; j < s2_length; j++) {
+        concatenated[i + j] = s2[j];
+    }
+
+    concatenated[i + j] = '\0';
+
+    return concatenated;
+}
