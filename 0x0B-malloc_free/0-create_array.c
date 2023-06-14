b8e74401b90e15c650a2a51699cbaf86a6796374
jlzbrowning <jlzbrowning@gmail.com>
2023-06-14 06:43:19 -0700

    Script commit from gits.sh

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
-#include "main.h"
-#include "stdlib.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <main.h>
 
 /**
  * create_array - Creates an array of chars and initializes it with a specific char
@@ -10,15 +11,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *array;
+
 	if (size == 0)
 		return (NULL);
 
-	char *array = (char *)malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = c;
 
 	return (array);
 }
+
