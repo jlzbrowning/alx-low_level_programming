6678e42489297dc239468787c597160dfcba3de9
jlzbrowning <jlzbrowning@gmail.com>
2023-06-14 06:48:54 -0700

    Script commit from gits.sh

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,18 +9,20 @@
  */
 char *_strdup(char *str)
 {
+	unsigned int length = 0, i;
+	char *duplicate;
+
 	if (str == NULL)
 		return (NULL);
 
-	unsigned int length = 0;
 	while (str[length] != '\0')
 		length++;
 
-	char *duplicate = (char *)malloc(sizeof(char) * (length + 1));
+	duplicate = (char *)malloc(sizeof(char) * (length + 1));
 	if (duplicate == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i <= length; i++)
+	for (i = 0; i <= length; i++)
 		duplicate[i] = str[i];
 
 	return (duplicate);
